eef1d339f78fb75d46c48852f95cfce40b7e5de2
Berk <berknorman@me.com>
2017-10-29 00:36:55 -0700

    Main function to run for generating thresholded semgentations

--- main_dicom_pipeline.py
+++ main_dicom_pipeline.py
@@ -1,148 +1,79 @@
 '''
 Berk Norman
-Pipeline for reading in dicoms and corresponding contour data
+Pipeline for reading in dicoms and corresponding inner and outer left ventricle contour data
+Also create thresholding classifier for blood pool given the outline of the heart muscle
 '''
-import dicom_parse_functions as parse
-import csv
-import os
-from os.path import isfile, join
-import numpy as np
-import random
+from __future__ import division
+import general_load_functions as dload
 import sys
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import pylab
+import numpy as np
+import scipy.stats as stat
+import copy
 
 
-def create_link_dict(dicom_directory, contour_directory, link_csv):
+def normalize01(array):
     '''
-    :param dicom_directory: directory of where dicom folders are stored
-    :param contour_directory: directory of where contour folders are stored
-    :param link_csv: csv containing the links between dicom folders and contour folders
-    :return: dictionary of respective .dcm files and their corresponding contour .txt files
+
+    :param array: any type of numpy array
+    :return: input array normalized to be between 0-1
     '''
-    directory_link={}
-    with open(link_csv, 'rb') as f:
-        reader = csv.reader(f)
-        #Skip the headers of the .csv file
-        reader.next()
-        for row in reader:
-            directory_link[os.path.join(dicom_directory + row[0])] = os.path.join(contour_directory + row[1],
-                                                                                        'i-contours/')
-        dicom_contour_pairs={}
-        for key, val in directory_link.items():
-            contour_files = [val + t for t in os.listdir(val) if isfile(join(val, t)) and t.endswith('.txt')]
-            # Extract the slice the contour file corresponds to by taking the 4 digits before the 'icontour' in the
-            # contour path file name
-            contour_slices = [int(s[s.find('icontour')-5:s.find('icontour')-1]) for s in contour_files]
-            for d in os.listdir(key):
-                dicom_file = join(key, d)
-                if isfile(dicom_file) and dicom_file.endswith('.dcm'):
-                    dicom_num = int(d[:-4])
-                    #Match dicom file slice with contour slice number, print to log if the dicom slice does not have
-                    # corresponding contour file
-                    try:
-                        contour_ind = contour_slices.index(dicom_num)
-                        dicom_contour_pairs[dicom_file]=contour_files[contour_ind]
-                    except:
-                        print('%s does not appear to have corresponding contour file' % dicom_file)
-    return dicom_contour_pairs
+    return (array-np.min(array))/(np.max(array)-np.min(array))
 
-class batch_generation:
+def intensity_extract(dicom_array, joint_mask, normalize=True):
     '''
-    Class that allows for batch generation of dicom and corresponding contours.
-    Also keeps track of how many epochs have been completed given the number of times the batch generation function
-    has been called.
+
+    :param dicom_array: array from dicom file
+    :param joint_mask: joint mask with area between heart muscle and blood pool labelled as 1 and blood pool labelled as
+    2
+    :param normalize: whether or not to normalize dicom array. Default is set to true
+    :return:
     '''
-    epochs_completed = 0
-    new_batches_completed = 0
-    start = 0
-    all_dicoms = []
-    image_count = 0
-    def __init__(self, batch_size):
-        self.batch_size = batch_size
+    if normalize:
+        dicom_array = normalize01(dicom_array)
+    outer_intensities = dicom_array[joint_mask==1]
+    inner_intensities = dicom_array[joint_mask==2]
+    return outer_intensities, inner_intensities
 
-    def load_dicom_mask(self, dicom_file_dir, contour_file_dir, image_saving = False):
-        ''' General function to load dicoms and contours into arrays
+def threshhold_seg(dicom_array, joint_mask, threshold, normalize=True):
+    '''Function that will create predicted left ventricle segmentation masks just based on thresholding and will return
+    the dice coefficient between the true left ventricle mask and the predicted one done through thresholding
+    
+    :param dicom_array: array from dicom file
+    :param joint_mask: joint mask with area between heart muscle and blood pool labelled as 1 and blood pool labelled as
+    2
+    :param threshold: intensity value with which all values great than in the entire heart muscle region will be
+    labelled as blood pool
+    :param normalize: whether or not to normalize dicom array. Default is set to true
+    :return:
+    '''
+    if normalize:
+        dicom_array = normalize01(dicom_array)
+    pred_mask = copy.copy(dicom_array)
+    true_mask = copy.copy(joint_mask)
+    #Remove all itensity surrounding o-contour segmentations
+    pred_mask[joint_mask==0] = 0
+    #Remove all o-contour below threshold to get predicted i-contour
+    pred_mask[pred_mask<threshold]=0
+    pred_mask[pred_mask!=0]=1
+    true_mask[true_mask==1]=0
+    true_mask[true_mask==2]=1
+    dice_val = dice_coef(true_mask, pred_mask)
+    return true_mask, pred_mask, dice_val
 
-        :param dicom_file_dir: file path to dicom file
-        :param contour_file_dir: file path to contour txt file corresponding to dicome file
-        :param image_saving: specify if images of dicoms with contour masks should be saved for examination of validity
-        of contour parsing functions
-        :return: dicom_array is the 256 x256 array representing the MRI slice, contour_mask is a binary mask
-                separating "the left ventricular blood pool from the heart muscle"
+def dice_coef(true_mask, pred_mask):
+    '''
 
-        '''
-        dicom_array = parse.parse_dicom_file(dicom_file_dir)
-        dicom_array = dicom_array['pixel_data']
-        height = dicom_array.shape[0]
-        width = dicom_array.shape[1]
-        if height!=256 and width!=256:
-            print('%s does not have correct 225 x 225 dimensions. Potentially look into')
-        contour_coords = parse.parse_contour_file(contour_file_dir)
-        contour_mask = parse.poly_to_mask(contour_coords, height, width)
-        contour_mask = np.asarray(contour_mask, dtype=np.int)
-        if image_saving:
-            contour_mask = np.ma.masked_where(contour_mask == 0, contour_mask)
-            plt.imshow(dicom_array, cmap=mpl.cm.bone)
-            plt.imshow(contour_mask, cmap=mpl.cm.jet_r, interpolation='nearest')
-            pylab.savefig(str(self.image_count) + '.png')
-            self.image_count += 1
-        return dicom_array, contour_mask
-
-    def data_shuffler(self, dicom_contour_pairs):
-        '''
-
-        :param dicom_contour_pairs: dictionary of all dicom file paths and their corresponding contour file paths.
-        This function is only called on the first iteration of each new epoch. It randomly shuffles all of the data and
-        randomly removes the remainder of data set size / batch size
-        '''
-        n_data = len(dicom_contour_pairs)
-        self.all_dicoms = list(dicom_contour_pairs.keys())
-        random.shuffle(self.all_dicoms)
-        extra_data = n_data % self.batch_size
-        if extra_data != 0:
-            print(
-            'Warning: batch size is not divisble by total number of data points. Randomly deleting remainder %d iterations for this epoch' % extra_data)
-            for i in range(extra_data):
-                self.all_dicoms.remove(random.choice(self.all_dicoms))
-
-    def new_batch(self, dicom_contour_pairs, image_saving):
-        ''' Function that actually generates batch array for input images and output masks, both of size
-        256 x 256 x batch size
-
-        :param dicom_contour_pairs: dictionary of all dicom file paths and their corresponding countoru file paths
-        :param image_saving: specify if images of dicoms with contour masks should be saved for examination of validity
-        of contour parsing functions
-        :return: input_batch is an array of batch size # of stacked images and output_batch is the stacked masks
-        '''
-        n_data = len(dicom_contour_pairs)
-        batches_to_complete = n_data/self.batch_size
-        #This randomly shuffles the data at the beggining of each new epoch to meet the requirements of Part 2: #3
-        if self.new_batches_completed ==0:
-            self.data_shuffler(dicom_contour_pairs)
-            self.start = 0
-
-        first=True
-        for b in range(self.start, self.start + self.batch_size):
-            dicom_array, contour_mask = self.load_dicom_mask(self.all_dicoms[b], dicom_contour_pairs[self.all_dicoms[b]], image_saving)
-            dicom_array = np.expand_dims(dicom_array, axis=-1)
-            contour_mask = np.expand_dims(contour_mask, axis=-1)
-            if first:
-                input_batch = dicom_array
-                output_batch = contour_mask
-                first = False
-            else:
-                input_batch = np.concatenate((input_batch, dicom_array), axis=-1)
-                output_batch = np.concatenate((output_batch, contour_mask), axis=-1)
-        self.start += self.batch_size
-        self.new_batches_completed+=1
-        #Restart batch count once epoch is complete
-        if self.new_batches_completed==batches_to_complete:
-            self.new_batches_completed=0
-            self.epochs_completed+=1
-            print('%d epoch complete' % self.epochs_completed)
-        return input_batch, output_batch
+    :param true_mask: true contour array mask
+    :param pred_mask: predicted contour array mask
+    :return: dice coefficient between the true mask and prediction mask, which is essentially just a measure of overlap
+    between the two masks
+    '''
+    truth = np.ndarray.flatten(true_mask)
+    pred = np.ndarray.flatten(pred_mask)
+    intersection = np.sum(np.multiply(truth, pred))
+    pred_card = len(pred[pred!=0])
+    truth_card = len(truth[truth!=0])
+    return 2*intersection/(pred_card + truth_card)
 
 
 def main():
@@ -154,15 +85,46 @@ def main():
     contour_directory = '/Users/Berk/Downloads/final_data/contourfiles/'
     link_coding = '/Users/Berk/Downloads/final_data/link.csv'
 
-    #Create dictionary of dicom files and their corresponding contour files
-    dicom_contour_dict = create_link_dict(dicom_directory, contour_directory, link_coding)
+    #Create dictionary of dicom files and their corresponding i and o contour files
+    dicom_icontour_dict = dload.create_link_dict(dicom_directory, contour_directory, link_coding, 'icontour')
+    dicom_ocontour_dict = dload.create_link_dict(dicom_directory, contour_directory, link_coding, 'ocontour')
+    #Join dicom keys with both their i contour and o contour files
+    joint_contour_dict = [dicom_icontour_dict, dicom_ocontour_dict]
+    all_dicom_keys = list(set(dicom_icontour_dict.keys()) & set(dicom_ocontour_dict.keys()))
+    all_contour_dict = {}
+    for keys in all_dicom_keys:
+        all_contour_dict[keys] = list(d[keys] for d in joint_contour_dict)
 
+    #PHASE 1
     #Generate data batches os size batch_size
-    dicom_and_contour = batch_generation(batch_size=batch_size)
-    for p in range(len(dicom_contour_dict)/batch_size):
-        input_batch, output_batch = dicom_and_contour.new_batch(dicom_contour_dict, image_saving=True)
-    f.close()
+    dicom_and_contour = dload.batch_generation(batch_size=batch_size)
+    # for p in range(int(np.floor(len(all_contour_dict)/batch_size))):
+    #     input_batch, output_batch = dicom_and_contour.new_batch(all_contour_dict, image_saving=True)
 
+    #PHASE 2
+    all_outer_data=[]
+    all_inner_data=[]
+    all_dice = []
+    for dicom, contour in all_contour_dict.iteritems():
+        dicom_array, joint_mask = dicom_and_contour.load_dicom_mask(dicom, contour)
+        #extract intensities from o-contours and i-contours
+        o_int, i_int = intensity_extract(dicom_array, joint_mask)
+        all_outer_data = np.append(all_outer_data, o_int, axis = 0)
+        all_inner_data = np.append(all_inner_data, i_int, axis = 0)
+        #create predicitons based on threshold intensity
+        true_mask, pred_mask, dice_val = threshhold_seg(dicom_array, joint_mask, 0.14)
+        all_dice.append(dice_val)
+    print('T-test results comparing intensities inside blood pool (inside the i-contour) to those inside the '
+          'heart muscles (between i-contour and o-contour)')
+    print(stat.ttest_ind(all_outer_data, all_inner_data))
+
+    #Plot overlap of the distributions
+    dload.intensity_plot(all_outer_data, all_inner_data)
+    #Side by side comparison of a true mask and predicted mask
+    dload.side_by_side_mask_overlay(dicom_array, true_mask, pred_mask)
+    average_dice_val = np.mean(all_dice)*100
+    print('Average dice coefficient using thresholding segmetnations method: %.2f%%' % average_dice_val)
+    f.close()
 if __name__ == "__main__":
     main()
 
